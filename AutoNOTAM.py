1d585ed9f645891fc39c308ea3b775e9a0f6e7e7
johnathanfernandes <66898724+johnathanfernandes@users.noreply.github.com>
2020-11-19 07:55:48 +0400

    Pulls names from E field

--- AutoNOTAM.py
+++ AutoNOTAM.py
@@ -17,49 +17,46 @@ root.filename = filedialog.askopenfilename(initialdir=cwd,
                                            filetypes=(("Text files", "*.txt"),
                                                       ("all files", "*.*"))) # Accept file input from user
 
-
 root.destroy() # Close GUI window
 
 with open(root.filename, 'r') as file:
     data = file.read().replace('\n', ' ') # Read NOTAM txt
+data = str(re.sub("  "," ",data))
 
 #------------------ Define regular expressions
-eventtext = re.compile(r"E\) .*?Q\)")
-coordstext = re.compile(r"(([0-9]*[\.][0-9]+)|([0-9]*))(N |S )(([0-9]*[\.][0-9]+)|([0-9]+))(W|E)")
-circletext = re.compile(r"(AREA CIRCLE WITH RADIUS) ([\d]*[\.][\d]*|[\d]*) (M|NM) (CENTERED ON) (([0-9]*[\.][0-9]+)|([0-9]*))(N |S )(([0-9]*[\.][0-9]+)|([0-9]*))(W|E)")
-polytext = re.compile(r"(AREA BOUNDED BY LINES JOINING:((( [0-9]*[\.][0-9]+)|( [0-9]*))(N|S)(( [0-9]*[\.][0-9]+)|( [0-9]*))(W|E))*)")
+eventtext = re.compile(r"E\).*? \)")
+circletext = re.compile(r" E\)(.*?)(AREA CIRCLE WITH RADIUS) ([\d]*[\.][\d]*|[\d]*) (M|NM) (CENTERED ON) (([0-9]*[\.][0-9]+)|([0-9]*))(N |S )(([0-9]*[\.][0-9]+)|([0-9]*))(W|E)")
+polytext = re.compile(r" E\)(.*?)(AREA BOUNDED BY LINES JOINING:((( [0-9]*[\.][0-9]+)|( [0-9]*))(N|S)(( [0-9]*[\.][0-9]+)|( [0-9]*))(W|E))*)")
 floattext = re.compile(r"([0-9]*[\.][0-9]+)|([0-9]*)")
 
 #------------------ Search using regular expressions
 events = re.findall(eventtext, data) # Number of events
-coordinates = re.findall(coordstext, data) # Number of events with coordinates
 circles = re.findall(circletext, data) # Number of circles
 polygons = re.findall(polytext, data) # Number of polygons
 
 #------------------ Display to user
 print("Total number of events: ", len(events))
-print("Number of events with coordinates found: ", len(coordinates))
 print("Number of circles found: ", len(circles))
 print("Number of polygons found: ", len(polygons))
 
 #------------------ Convert circle coordinates to decimal
-for item in range(len(circles)):
-    name = str(item)
-    latdeg = float(circles[item][4][0:2])
-    latmin = float(circles[item][4][2:4])
-    latsec = float(circles[item][4][4:len(circles[item][4])])
+for index, item in enumerate(range(len(circles))):
+    name = str(re.sub("(.*?)E\)","",circles[item][0]))
+    latdeg = float(circles[item][5][0:2])
+    latmin = float(circles[item][5][2:4])
+    latsec = float(circles[item][5][4:len(circles[item][5])])
     lat = latdeg + (latmin/60) + (latsec/3600)
 
-    longdeg = float(circles[item][8][0:3])
-    longmin = float(circles[item][8][3:5])
-    longsec = float(circles[item][8][5:len(circles[item][8])])
+    longdeg = float(circles[item][9][0:3])
+    longmin = float(circles[item][9][3:5])
+    longsec = float(circles[item][9][5:len(circles[item][9])])
     long = longdeg + (longmin/60) + (longsec/3600)
 
 #------------------ Convert NM to M if present
-    if circles[item][2] == "NM":
-        rad = (float(circles[item][1]) * 1852.001)
+    if circles[item][3] == "NM":
+        rad = (float(circles[item][2]) * 1852.001)
     else:
-        rad = float(circles[item][1])
+        rad = float(circles[item][2])
     nov = math.ceil(rad/2)
 
 #------------------ Generate circle kml files
@@ -70,17 +67,17 @@ for item in range(len(circles)):
 
     pol = kml.newpolygon(name=name, outerboundaryis=polycircle.to_kml())
     pol.style.polystyle.color = simplekml.Color.changealphaint(200, simplekml.Color.green)
-    kml.save("Circle"+name+".kml")
+    kml.save("Circle"+str(index)+" "+name+".kml")
+    print("File Circle"+str(index)+": "+name+".kml saved")
 
 #------------------ Convert polyon coordinates to decimal and store in list
-filecount = 0
 
 coordslist = []
 
 for i in range(len(polygons)):
-    coordslist.append(polygons[i][0])
+    coordslist.append(polygons[i][1])
 
-for item in coordslist:
+for index, item in enumerate(coordslist):
     textlist = item[30:].split()
 
     boundary = []
@@ -99,8 +96,7 @@ for item in coordslist:
         coord = (long, lat)
         boundary.append(coord)
 
-    name = str(filecount)
-    filecount = filecount + 1
+    name = str(re.sub("(.*?)E\)","",polygons[index][0]))
 
 #------------------ Generate polygon kml files
     pol = kml.newpolygon(name = name)
@@ -108,7 +104,8 @@ for item in coordslist:
     pol.style.linestyle.color = simplekml.Color.green
     pol.style.linestyle.width = 5
     pol.style.polystyle.color = simplekml.Color.changealphaint(200, simplekml.Color.green)
-    kml.save("Polygon"+name+".kml")
+    kml.save("Polygon"+str(index)+" "+name+".kml")
+    print("File Polygon"+str(index)+": "+name+".kml saved")
 
 input("All files generated, press enter to close")
 
